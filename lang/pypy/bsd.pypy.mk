b059ea665d953707bded04dd2f987b12debb02f5
David Naylor <naylor.b.david@gmail.com>
2017-05-01 20:01:10 +0200

    Add commit r439901

--- lang/pypy/bsd.pypy.mk
+++ lang/pypy/bsd.pypy.mk
@@ -1,4 +1,4 @@
-# $FreeBSD: head/lang/pypy/bsd.pypy.mk 419600 2016-08-04 08:43:36Z dbn $
+# $FreeBSD: head/lang/pypy/bsd.pypy.mk 439901 2017-05-01 18:00:57Z dbn $
 
 .if !defined(LICENSE)
 LICENSE=	MIT PSFL
@@ -7,6 +7,6 @@ LICENSE_COMB=	multi
 
 USES+=		compiler:c11
 
-PYPY_DIR=	pypy-${PORTVERSION:C|([0-9])\.([0-9]).*|\1.\2|}
+PYPY_DIR=	${PORTNAME}-${PORTVERSION:C|([0-9])\.([0-9]).*|\1.\2|}
 PYPY_CFFI_VER?=	pypy-41
 PLIST_SUB+=	PYPY_DIR=${PYPY_DIR} PYPY_CFFI_VER=${PYPY_CFFI_VER}
