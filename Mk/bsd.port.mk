c1a87983e3cfd8a0ffbb45dbba420dc462de757e
David Naylor <naylor.b.david@gmail.com>
2017-06-18 20:09:48 +0200

    Add commit r443845

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1,7 +1,7 @@
 #-*- tab-width: 4; -*-
 # ex:ts=4
 #
-# $FreeBSD: head/Mk/bsd.port.mk 443599 2017-06-14 20:18:38Z tcberner $
+# $FreeBSD: head/Mk/bsd.port.mk 443845 2017-06-18 18:09:16Z dbn $
 #	$NetBSD: $
 #
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
@@ -225,6 +225,10 @@ FreeBSD_MAINTAINER=	portmgr@FreeBSD.org
 #				  Default: not set.
 #
 # NO_ARCH			- Set this if port is architecture neutral.
+#
+# NO_ARCH_IGNORE		- Set this to a list files to ignore when NO_ARCH is checked
+# 				  in stage-qa (i.e. architecture specific files that are
+# 				  'bundled' with the port).
 #
 # Set these if your port only makes sense to certain architectures.
 # They are lists containing names for them (e.g., "amd64 i386").
@@ -1527,7 +1531,9 @@ QA_ENV+=		STAGEDIR=${STAGEDIR} \
 				PKGORIGIN=${PKGORIGIN} \
 				LIB_RUN_DEPENDS='${_LIB_RUN_DEPENDS:C,[^:]*:([^:]*):?.*,\1,}' \
 				UNIFIED_DEPENDS=${_UNIFIED_DEPENDS:C,([^:]*:[^:]*):?.*,\1,:O:u:Q} \
-				PKGBASE=${PKGBASE}
+				PKGBASE=${PKGBASE} \
+				NO_ARCH=${NO_ARCH} \
+				"NO_ARCH_IGNORE=${NO_ARCH_IGNORE}"
 .if !empty(USES:Mssl)
 QA_ENV+=		USESSSL=yes
 .endif
