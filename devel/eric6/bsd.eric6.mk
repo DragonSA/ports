e560f53f4fa58f8c0c1b27f77ff64d04311aa7af
David Naylor <naylor.b.david@gmail.com>
2019-04-07 19:00:48 +0200

    Add commit r498296

--- devel/eric6/bsd.eric6.mk
+++ devel/eric6/bsd.eric6.mk
@@ -1,12 +1,12 @@
 # Created by: Boris Samorodov <bsam@FreeBSD.org>
-# $FreeBSD: head/devel/eric6/bsd.eric6.mk 497427 2019-03-31 16:15:36Z dbn $
+# $FreeBSD: head/devel/eric6/bsd.eric6.mk 498296 2019-04-07 16:59:03Z dbn $
 #
 # This is a set of Makefile macros that are common between devel/eric6 and
 # localized language packages for devel/eric6:
 # german/eric6, russian/eric6, devel/es-eric6
 
 PORTNAME=	eric6
-PORTVERSION=	19.03
+PORTVERSION=	19.04
 MASTER_SITES=	SF/eric-ide/${PORTNAME}/stable/${PORTVERSION}
 WRKSRC=		${WRKDIR}/${PORTNAME}-${PORTVERSION}
 
